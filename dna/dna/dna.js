a07823d8e23acc79cdc257042b9841adc7aec5eb
theramstoss <eomseongj@gmail.com>
2020-08-23 07:22:29 -0600

    first try with papa

--- dna/dna/dna.js
+++ dna/dna/dna.js
@@ -1,29 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').onsubmit = function() {
-        const sequenceFile = document.getElementById('sequence').files[0];
-        const databaseFile = document.getElementById('database').files[0];
-        var sequenceReader = new FileReader();
-        
+import Papa from 'dna/node_modules/papaparse/papaparse.js'
 
-        function loadSequence() {
-            return new Promise(resolve => {
-                sequenceReader.addEventListener('load', resolve, {once:true});
-            })
-        }
+document.querySelector('form').onsubmit = function() {
+    const sequenceFile = document.getElementById('sequence').files[0];
+    const databaseFile = document.getElementById('database').files[0];
+    var sequenceReader = new FileReader();
+    
 
-        // function loadDatabase() {
-        //     return new Promise(resolve => {
-
-        //     })
-        // }
-
-        loadSequence().then(e => console.log(e));
-
-        // reader.onload = function(e) {
-        //     let sequence = e.target.result;
-        //     document.querySelector('#p1').innerHTML = sequence;
-        // }
-        sequenceReader.readAsText(sequenceFile);
-        return false;
+    function loadSequence() {
+        return new Promise(resolve => {
+            sequenceReader.addEventListener('load', resolve, {once:true});
+        })
     }
-})
+
+    Papa.parse(databaseFile, {complete: function() {console.log('success!')}})
+
+    // function loadDatabase() {
+    //     return new Promise(resolve => {
+
+    //     })
+    // }
+
+    loadSequence().then(e => console.log(e));
+
+    // reader.onload = function(e) {
+    //     let sequence = e.target.result;
+    //     document.querySelector('#p1').innerHTML = sequence;
+    // }
+    sequenceReader.readAsText(sequenceFile);
+    return false;
+}
